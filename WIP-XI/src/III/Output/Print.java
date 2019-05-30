bcb96103dfd59a3ab5156d6588856d48109becc4
oat2521 <oat-91@hotmail.com>
2019-05-30 11:38:05 +0700

    [ADD] New Project

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("โอ๊ต พระราม7");
+        
+        System.out.println("รส ซากุระ,นารูโต๊ะ ซาซู");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.println("นาย ลูกน้องโก๋จ๊อบ");
+        
+       // System.out.print("อ");
+        
+    }
+}
